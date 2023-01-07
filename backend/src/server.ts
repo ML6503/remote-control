03239a7e05778c71b6e84f55cd11b696ad572701
ML6503 <ml6503@gmail.com>
2023-01-07 19:06:08 +0200

    feat: add websocket connection to BE and FE

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import { WebSocketServer } from "ws";
+import * as dotenv from 'dotenv'; 
+dotenv.config();
+
+const PORT = +process.env.PORT | 8080;
+
+const ws = new WebSocketServer({port: PORT});
+console.log('Server is running on ', `ws://localhost:${PORT}`);
+
+ws.on('connection', client => {
+ // console.log('Server is running on ', `ws://localhost:${PORT}`);
+  client.on('message', (data) => {
+  
+    console.log('received message from client:', data.toString('utf8'));
+
+    // [...ws.clients].filter(c => c !== client).forEach(c => c.send(isBinary ? message.toString() : message));
+  });
+
+  client.send('Hello from server ws!');
+});
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import { httpServer } from "./src/http_server/index.js";
 import { mouse } from "@nut-tree/nut-js";
+import WebSocket from "ws";
 
 const HTTP_PORT = 8181;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const ws = new WebSocket(`ws://localhost:${WS_PORT}`);
+ws.on('open', () => {
+  console.log(`[Client] Connected`);
+  ws.send(`Hi from the client!`);
+});
+
+ws.on('message', (data) => {  
+  console.log('Received msg from server:' + data);
+});
